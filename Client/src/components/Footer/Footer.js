8084d232b48cee7af4b196e49385f8b3bbb9f0ff
moamen alaa <moamenalaa21@gmail.com>
2021-12-22 17:46:23 +0200

    footer comp

---
+++ Client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../css/Footer/Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            &copy; copyright MoamenAmer 2021
+        </footer>
+    )
+}
+export default Footer
