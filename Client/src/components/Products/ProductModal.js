77e1e56a17f1070ab330e3c8b0e26189d35ceee2
moamen alaa <moamenalaa21@gmail.com>
2021-12-23 17:23:56 +0200

    Product detalis

---
+++ Client/src/components/Products/ProductModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+function ProductModal(props) {
+    const { product, closeModal } = props;
+    return (
+        <Modal isOpen={product} onRequestClose={closeModal}>
+            <span className="close-icon" onClick={closeModal}>&times;</span>
+            <div className="product-info">
+                <img src={product.imageurl} alt={product.title} />
+                <p> {product.title}</p>
+                <p>{product.desc}</p>
+            </div>
+        </Modal>
+    )
+}
+export default ProductModal
