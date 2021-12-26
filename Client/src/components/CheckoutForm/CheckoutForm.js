fdf52447bcdce0fa5918e83a77e9d6f324f0e727
moamen alaa <moamenalaa21@gmail.com>
2021-12-26 20:35:25 +0200

    Add Anmi part2

--- Client/src/components/CheckoutForm/CheckoutForm.js
+++ Client/src/components/CheckoutForm/CheckoutForm.js
@@ -1,32 +1,37 @@
 import React, { Fragment } from 'react'
 import "../../css/CheckoutForm/CheckoutForm.css"
 import Input from '../Input/Input'
+import Zoom from 'react-reveal/Zoom'
 
 
 function CheckoutForm(props) {
     return (
+
         <Fragment>
             {props.showForm &&
 
                 <div className="checkout-form">
                     <span className="close-icon" onClick={() => props.setShowForm(false)}>&times; </span>
-                    <form onSubmit={props.submitOrder}>
-                        <Input
-                            label="name"
-                            type="text"
-                            onChange={props.handleChange}
-                            name="name" />
-                        <Input
-                            label="email"
-                            type="email"
-                            onChange={props.handleChange}
-                            email="email" />
-                        <div>
-                            <button type="submit">submit</button>
-                        </div>
-                    </form>
+                    <Zoom>
+                        <form onSubmit={props.submitOrder}>
+                            <Input
+                                label="name"
+                                type="text"
+                                onChange={props.handleChange}
+                                name="name" />
+                            <Input
+                                label="email"
+                                type="email"
+                                onChange={props.handleChange}
+                                email="email" />
+                            <div>
+                                <button type="submit">submit</button>
+                            </div>
+                        </form>
+                    </Zoom>
                 </div>}
         </Fragment>
+
     )
 }
 export default CheckoutForm
