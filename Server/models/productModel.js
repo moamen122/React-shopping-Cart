7971176dc377d50ad113439b0ae9e4dde347444d
moamen alaa <moamenalaa21@gmail.com>
2021-12-27 01:34:39 +0200

    setup server

---
+++ Server/models/productModel.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const productScheam = require('../schema/productSchema');
+
+const Product = mongoose.model("product", productScheam);
+module.exports = Product
