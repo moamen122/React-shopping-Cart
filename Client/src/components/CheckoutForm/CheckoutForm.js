76c856992af59a047447a17654b9e360fab22ea4
moamen alaa <moamenalaa21@gmail.com>
2021-12-26 17:37:13 +0200

    checkout comp

--- Client/src/components/CheckoutForm/CheckoutForm.js
+++ Client/src/components/CheckoutForm/CheckoutForm.js
@@ -1,10 +1,36 @@
 import React from 'react'
-
-function CheckoutForm() {
+import "../../css/CheckoutForm/Checkout.css"
+import Input from '../Input/Input'
+import Zoom from 'react-reveal/Zoom'
+import { words } from '../../words'
+function Checkout(props) {
     return (
-        <div>
+        <>
+            {props.showForm && <div className="checkout-form">
+                <span className="close-icon" onClick={() => props.setShowForm(false)}> &times; </span>
+                <Zoom left>
+                    <form onSubmit={props.submitOrder}>
+                        <Input
+                            label={words.name}
+                            type="text"
+                            name="name"
+                            onChange={props.handleChange}
+                        />
+                        <Input
+                            label="Email"
+                            type="email"
+                            onChange={props.handleChange}
+                            name="email"
+                        />
 
-        </div>
+                        <div>
+                            <button type="submit"> {words.checkout} </button>
+                        </div>
+                    </form>
+                </Zoom>
+            </div>}
+        </>
     )
 }
-export default CheckoutForm
+
+export default Checkout
